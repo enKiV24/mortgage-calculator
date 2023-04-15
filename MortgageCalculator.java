db68adb0ae1b231ce9d29abb924927a33768203b
Andrzej Kornaś <enKiV24@gmail.com>
2023-04-15 18:01:47 +0200

    mortgage-calculator.v0.2

--- MortgageCalculator.java
+++ MortgageCalculator.java
@@ -26,8 +26,6 @@ public class MortgageCalculator {
 
     public static void main(String[] args) {
 
-        // To jest rodzaj mapy, który w tym przypadku trzyma klucze posortowane rosnąco.
-        // Wiem, że nie tłumaczyłem go wcześniej. Spokojnie, jeszcze będzie ;)
         Map<Integer, BigDecimal> overpaymentSchema = new TreeMap<>();
         overpaymentSchema.put(5, BigDecimal.valueOf(12000));
         overpaymentSchema.put(19, BigDecimal.valueOf(10000));
@@ -35,7 +33,6 @@ public class MortgageCalculator {
         overpaymentSchema.put(64, BigDecimal.valueOf(16000));
         overpaymentSchema.put(78, BigDecimal.valueOf(18000));
 
-        // Ważne jest to aby zwrócić uwagę, na to, które dane wejściowe są nadpisywane przez withery
         InputData defaultInputData = new InputData()
             .withAmount(new BigDecimal("296192.11"))
             .withMonthsDuration(BigDecimal.valueOf(360))
